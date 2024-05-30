c16356f4084470a1ef6e69579f813bf1e6efcc46
MatheusVitorFerreira <matheusvitor.dev00@gmail.com>
2024-05-30 17:24:14 -0300

    Criacao do projeto E o eureka para conectar os microservicos

---
+++ eurekaserver/src/test/java/edu/microservicos/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.microservicos.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
